d0ea594e6aa4be168e4a8e191c7069259bea1a7f
Victor Kensy <victorkensy@gmail.com>
2024-11-04 14:27:40 -0300

    feat: create economy system and a way to bless or curse user

--- src/cogs/economia.py
+++ src/cogs/economia.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -51,6 +52,118 @@ class Economia(commands.Cog):
         self.usuarios.update_one({"user_id": str(destinatario.id)}, {"$inc": {"saldo": quantidade}}, upsert=True)
 
         await interaction.response.send_message(f"{interaction.user.mention} enviou 👻 {quantidade} almas para {destinatario.mention}.")
+
+    @app_commands.command(name="amaldicoar", description="Amaldiçoe um membro do servidor com o poder do Celestial Ynsek para roubar almas dele.")
+    @app_commands.describe(membro="Usuário que receberá a maldição")
+    async def amaldicoar(self, interaction: discord.Interaction, membro: discord.User):
+        image_url = "https://pa1.aminoapps.com/6587/bd57157214be76a2c814f2eeb6d8017b66cad806_hq.gif"
+        try:
+            user_data = self.db.users.find_one({"user_id": str(interaction.user.id)})
+            target_data = self.db.users.find_one({"user_id": str(membro.id)})
+            if user_data is None or target_data is None:
+                await interaction.response.send_message("Um dos usuários não foi encontrado no banco de dados.", ephemeral=True)
+                return
+
+            last_curse_time = user_data.get("last_curse_time")
+            if last_curse_time:
+                last_curse_time = datetime.fromisoformat(last_curse_time)
+                if datetime.utcnow() < last_curse_time + timedelta(hours=12):
+                    await interaction.response.send_message("Você só pode amaldiçoar a cada 12 horas.", ephemeral=True)
+                    return
+
+            if target_data["saldo"] <= 0:
+                await interaction.response.send_message(f"O usuário <@{membro.id}> não tem saldo para roubar.", ephemeral=True)
+                return
+
+            # Gera um número aleatório entre 1 e 30
+            amount_to_steal = random.randint(1, 30)
+
+            if target_data["saldo"] < amount_to_steal:
+                amount_to_steal = target_data["saldo"]
+
+            self.db.users.update_one(
+                {"user_id": str(interaction.user.id)},
+                {"$inc": {"saldo": amount_to_steal}, "$set": {"last_curse_time": datetime.utcnow().isoformat()}}
+            )
+            self.db.users.update_one(
+                {"user_id": str(membro.id)},
+                {"$inc": {"saldo": -amount_to_steal}}
+            )
+
+            if amount_to_steal == 30:
+                message = f"O membro <@{interaction.user.id}> agradou o celestial Ynsek ao realizar um ritual de maldição ao membro <@{membro.id}> e foi presenteado com 30 almas do amaldiçoado."
+                image_url = "https://i.pinimg.com/originals/95/db/bf/95dbbfb1553ee9a04cff20402ae86ed1.gif"
+            else:
+                message = f'O Membro <@{interaction.user.id}> realizou um ritual de maldição ao membro <@{membro.id}> e roubou {amount_to_steal} almas com o auxílio dos poderes do Celestial Ynsek.'
+        except Exception as e:
+            print(f'Erro ao realizar a parte de conexão: {e}')        
+        try:
+            embed = discord.Embed(
+                colour=discord.Color.red(),
+                title="O poder do Celestial Ynsek ressurge",
+                description=message
+            )
+
+            embed.set_image(url=image_url)
+
+            await interaction.channel.send(embed=embed)
+            return
+        except Exception as e:
+            print(f'Erro ao realizar o embed: {e}')
+                    
+    @app_commands.command(name="abencoar", description="Abençoe um membro do servidor para compartilhar almas do Celestial Leirrac.")
+    @app_commands.describe(membro="Usuário que receberá a benção")
+    async def abencoar(self, interaction: discord.Interaction, membro: discord.User):
+        image_url = "https://64.media.tumblr.com/715a9e19350966145612992438eb51fc/d0a52de489aa8949-b0/s500x750/f2a33c991f201a0b99f084c04c703e98d7d51428.gif"
+
+        try:
+                user_data = self.db.users.find_one({"user_id": str(interaction.user.id)})
+                target_data = self.db.users.find_one({"user_id": str(membro.id)})
+
+                if user_data is None or target_data is None:
+                    await interaction.response.send_message("Um dos usuários não foi encontrado no banco de dados.", ephemeral=True)
+                    return
+
+                last_bless_time = user_data.get("last_bless_time")
+                if last_bless_time:
+                    last_bless_time = datetime.fromisoformat(last_bless_time)
+                    if datetime.utcnow() < last_bless_time + timedelta(hours=12):
+                        await interaction.response.send_message("Você só pode abençoar a cada 12 horas.", ephemeral=True)
+                        return
+
+                # Gera um número aleatório entre 1 e 30 para a bênção
+                amount_to_bless = random.randint(1, 30)
+
+                # Atualiza o saldo de ambos os usuários com o valor de bênção e registra o horário
+                self.db.users.update_one(
+                    {"user_id": str(interaction.user.id)},
+                    {"$inc": {"saldo": amount_to_bless}, "$set": {"last_bless_time": datetime.utcnow().isoformat()}}
+                )
+                self.db.users.update_one(
+                    {"user_id": str(membro.id)},
+                    {"$inc": {"saldo": amount_to_bless}}
+                )
+
+                # Mensagem com resultado da bênção
+                if amount_to_bless == 30:
+                    message = f"O membro <@{interaction.user.id}> invocou o celestial Leirrac que o abençoou e <@{membro.id}> com 30 almas cada!"
+                else:
+                    message = f"O Membro <@{interaction.user.id}> abençoou o membro <@{membro.id}> com o poder do celestial Leirrac com {amount_to_bless} almas para ambos!"
+
+        except Exception as e:
+                print(f'Erro ao realizar a conexão com o banco de dados: {e}')
+                return
+            
+        try:
+            embed = discord.Embed(
+                colour=discord.Color.green(),
+                title="O poder do Celestial Leirrac abençoa",
+                description=message
+            )
+            embed.set_image(url=image_url)
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            print(f'Erro ao enviar o embed: {e}')
 
 async def setup(bot):
     await bot.add_cog(Economia(bot))
--- src/main.py
+++ src/main.py
@@ -26,8 +26,8 @@ async def load_cogs():
 async def on_ready():
     await bot.change_presence(activity=discord.CustomActivity(name="👉 Servidor oficial | https://bit.ly/o-culto-discord"))
     try:
-        bot.tree.clear_commands(guild=discord.Object(id=1044018298951569498))
-        await bot.tree.sync(guild=discord.Object(id=1044018298951569498))
+        result = await bot.tree.sync()
+        # print(result)
         
     except Exception as e:
         print(f"Erro ao sincronizar slash commands: {e}")
@@ -36,11 +36,6 @@ async def on_ready():
 async def main():
     await load_cogs()
     await bot.start(DISCORD_BOT_TOKEN)
-    
-@bot.tree.command(name="ping", description="Testa o bot")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong!")
-
 
 import asyncio
 asyncio.run(main())
